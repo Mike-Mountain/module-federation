b0b217f4297fd21828d149a589fd6087ce52b313
Michael Mountain <michael.mountain@mmltd.co.za>
2021-06-18 17:01:20 +0200

    setting up module federation (WIP)

--- apps/dashboard/src/app/app-routing.module.ts
+++ apps/dashboard/src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import {NgModule} from '@angular/core';
 import {Route, RouterModule} from "@angular/router";
 import {LandingComponent} from "./core/components/landing/landing.component";
+import {loadRemoteModule} from "@angular-architects/module-federation";
 
 const routes: Route[] = [
   {path: 'landing', component: LandingComponent},
+  {
+    path: 'media',
+    loadChildren: () =>
+      loadRemoteModule({
+        remoteEntry: 'http://localhost:5000/remoteEntry.js',
+        remoteName: 'ChildAppOne',
+        exposedModule: './Module'
+      }).then(m => m.AppModule)
+  },
   {path: '', pathMatch: 'full', redirectTo: 'landing'}
 ];
 
